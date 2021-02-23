156856ed47e8320cd8312c25e5ea9e4facf672db
Milorad Radovic <miloradradovicmico98@gmail.com>
2021-02-23 15:50:10 +0100

    initialization

---
+++ intens-internship-spring-app/intens-internship/src/main/java/com/project/intensinternship/IntensInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.project.intensinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensInternshipApplication.class, args);
+	}
+
+}
