7e0c2f4caf77047592f99048b90307c027da511f
Milorad Radovic <miloradradovicmico98@gmail.com>
2021-02-25 13:46:50 +0100

    sql files for database initialization

---
+++ intens-internship-spring-app/intens-internship/src/main/resources/sql/candidates.sql
@@ -0,0 +1,10 @@
+INSERT INTO candidates (contact_number, date_of_birth, email, full_name) values ('+381621127650', '1998-04-25 00:00:00.000000', 'radovic.milorad1998@gmail.com', 'Milorad Radovic');
+INSERT INTO candidates (contact_number, date_of_birth, email, full_name) values ('+381621127651', '1992-05-26 00:00:00.000000', 'pera.peric@gmail.com', 'Pera Peric');
+INSERT INTO candidates (contact_number, date_of_birth, email, full_name) values ('+381621127652', '2000-03-15 00:00:00.000000', 'nikola.nikolic@gmail.com', 'Nikola Nikolic');
+INSERT INTO candidates (contact_number, date_of_birth, email, full_name) values ('+381621127653', '1993-04-03 00:00:00.000000', 'marko.markovic@gmail.com', 'Marko Markovic');
+INSERT INTO candidates (contact_number, date_of_birth, email, full_name) values ('+381621127654', '1994-12-11 00:00:00.000000', 'petar.petrovic@gmail.com', 'Petar Petrovic');
+INSERT INTO candidates (contact_number, date_of_birth, email, full_name) values ('+381621127655', '1995-11-12 00:00:00.000000', 'stefan.stefanovic@gmail.com', 'Stefan Stefanovic');
+INSERT INTO candidates (contact_number, date_of_birth, email, full_name) values ('+381621127656', '1996-10-25 00:00:00.000000', 'marija.markovic@gmail.com', 'Marija Markovic');
+INSERT INTO candidates (contact_number, date_of_birth, email, full_name) values ('+381621127657', '1997-09-13 00:00:00.000000', 'nikolina.nikolic@gmail.com', 'Nikolina Nikolic');
+INSERT INTO candidates (contact_number, date_of_birth, email, full_name) values ('+381621127658', '1998-08-17 00:00:00.000000', 'nina.visnjic@gmail.com', 'Nina Visnjic');
+INSERT INTO candidates (contact_number, date_of_birth, email, full_name) values ('+381621127659', '1999-07-01 00:00:00.000000', 'milica.milic@gmail.com', 'Milica Milic');
